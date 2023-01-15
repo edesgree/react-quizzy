1952bcce56ca146dae8fb2429871ac30c5c009b0
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-16 00:57:26 +0100

    get question from api call

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Question(props) {
   return (
     <div className="question">
-      <h2 className="question-title">How would one say goodbye in Spanish?</h2>
+      <h2 className="question-title">{props.question}</h2>
+      correct_answer{props.correct_answer}
       <div className="question-answers">
         <button className={`answer ${props.state}`}>Adiós</button>
         <button className={`answer ${props.state}`}>Hola</button>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,14 +2,68 @@ import React from 'react';
 import Question from './Question';
 
 export default function Quiz(props) {
+  const [quiz, setQuiz] = React.useState([]);
+  console.log('quiz', quiz);
+
+  React.useEffect(() => {
+    // get quiz from API
+    fetch('https://opentdb.com/api.php?amount=1')
+      .then((res) => res.json())
+      .then((data) => {
+        const quizElements = data.results;
+        console.log('data.results', data.results);
+        setQuiz(quizElements);
+      });
+    console.log('i fire once');
+  }, []);
+  function mergeAnswers(string, array) {
+    const allAnswers = array.push(string);
+    console.log('allAnswers', allAnswers);
+  }
   return (
     <section className="quiz">
       <div className="quiz-questions">
-        <Question state="" />
-        <Question state="desactive" />
-        <Question state="desactive correct" />
-        <Question state="desactive wrong" />
-        <Question state="active" />
+        {quiz.map((item) => {
+          console.log('item', item.question);
+
+          return (
+            <Question
+              key={item}
+              question={item.question}
+              correct_answer={item.correct_answer}
+              all_answers={mergeAnswers(
+                item.correct_answer,
+                item.incorrect_answer
+              )}
+            />
+          );
+        })}
+        ****************************
+        <Question
+          question="How would one say goodbye in Spanish?"
+          correct_answer="correct_answer"
+          state=""
+        />
+        <Question
+          question="How would one say goodbye in Spanish?"
+          correct_answer="correct_answer"
+          state="desactive"
+        />
+        <Question
+          question="How would one say goodbye in Spanish?"
+          correct_answer="correct_answer"
+          state="desactive correct"
+        />
+        <Question
+          question="How would one say goodbye in Spanish?"
+          correct_answer="correct_answer"
+          state="desactive wrong"
+        />
+        <Question
+          question="How would one say goodbye in Spanish?"
+          correct_answer="correct_answer"
+          state="active"
+        />
       </div>
       <footer className="quiz-footer">
         <button className="primary">Check answers</button>
