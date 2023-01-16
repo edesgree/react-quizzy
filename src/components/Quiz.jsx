09552dc0d9180fcd6784ade78b1199285dfa989e
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-16 03:19:07 +0100

    loading sign

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,6 +3,7 @@ import Question from './Question';
 
 export default function Quiz(props) {
   const [quiz, setQuiz] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     // get quiz from API
@@ -11,6 +12,7 @@ export default function Quiz(props) {
       .then((data) => {
         // set current quiz with result from api
         setQuiz(data.results);
+        setLoading(false);
         console.log('quiz', quiz);
         console.log('data.results', data.results);
       });
@@ -25,25 +27,28 @@ export default function Quiz(props) {
   }
   return (
     <section className="quiz">
-      <div className="quiz-questions">
-        {quiz.map((item) => {
-          console.log('item', item.question);
+      {loading ? (
+        'loading...'
+      ) : (
+        <div className="quiz-questions">
+          {quiz.map((item) => {
+            console.log('item', item.question);
 
-          return (
-            <Question
-              key={item}
-              question={item.question}
-              correct_answer={item.correct_answer}
-              incorrect_answers={item.incorrect_answers}
-              all_answers={mergeAnswers(
-                item.correct_answer,
-                item.incorrect_answers
-              )}
-            />
-          );
-        })}
-        ****************************
-        {/*
+            return (
+              <Question
+                key={item}
+                question={item.question}
+                correct_answer={item.correct_answer}
+                incorrect_answers={item.incorrect_answers}
+                all_answers={mergeAnswers(
+                  item.correct_answer,
+                  item.incorrect_answers
+                )}
+              />
+            );
+          })}
+          ****************************
+          {/*
         <Question
           question="How would one say goodbye in Spanish?"
           correct_answer="answer 1"
@@ -80,7 +85,8 @@ export default function Quiz(props) {
           state="active"
         /> 
         */}
-      </div>
+        </div>
+      )}
       <footer className="quiz-footer">
         <button className="primary">Check answers</button>
         <p>You scored 3/5 correct answers</p>
