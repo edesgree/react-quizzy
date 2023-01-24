3e9f9c648f1e935e5e29265468e7ee4d62ea077b
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-24 02:24:02 +0100

    css color update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Intro(props) {
   return (
     <div>
-      <p>Let's play</p>
+      <p>Let's play a random trivia!</p>
       <button className="primary" onClick={props.start}>
         Start Quiz
       </button>
