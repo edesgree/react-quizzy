ec5deee3d7eafd0f4c348004d2359ac51e704bfc
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-24 01:42:21 +0100

    cleanup console log

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,26 +4,21 @@ import { decode } from 'html-entities';
 
 function Question(props) {
   const [choice, setChoice] = React.useState('');
-  const [active, setActive] = React.useState(false);
-  const [correct, setCorrect] = React.useState(false);
   function handleChoice(event) {
     event.preventDefault();
-    setChoice(event.target.value);
-    console.log('my choice is: ', choice);
-    setCorrect(checkCorrectAnswer(event.target.value, props.correct_answer));
-    console.log('my correctness is: ', correct);
-    // update quiz object with choice
-    props.updateUserChoice(props.id, event.target.value);
+    if (!props.quiz_completed) {
+      setChoice(event.target.value);
+
+      // update quiz object with choice
+      props.updateUserChoice(props.id, event.target.value);
+    }
   }
 
   React.useEffect(() => {
-    console.log('click');
+    console.log('un choix a ete fait');
     console.log('props.quiz_completed', props.quiz_completed);
   }, [choice]);
-  function checkCorrectAnswer(choice, correct_answer) {
-    return choice === correct_answer;
-  }
-  console.log('props.all_answers ', props.all_answers);
+
   const answersElements = props.all_answers?.map((answer) => {
     function getButtonStyle() {
       let style = null;
@@ -55,15 +50,10 @@ function Question(props) {
       </button>
     );
   });
-  const AllElements = props.all_answers?.map((item) => `${item} | `);
   return (
     <div className="question">
       <h2 className="question-title">{props.question}</h2>
       correct_answer: {props.correct_answer}
-      <br />
-      all answer: {AllElements}
-      <br />
-      user choice : {choice} | answer is {correct ? 'good' : 'wrong'}
       <div className="question-answers">{answersElements}</div>
     </div>
   );
